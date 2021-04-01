89b0c27a98ab4c9ce165e94bd241f00e191456af
Zuraiz Ahmed Shehzad <zuraizahmed1999@gmail.com>
2021-04-02 04:10:51 +0500

    Firebase Connection 1

--- app/src/main/java/com/fyp/fypui/signup_form.java
+++ app/src/main/java/com/fyp/fypui/signup_form.java
@@ -3,9 +3,22 @@ package com.fyp.fypui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class signup_form extends AppCompatActivity {
-
+    public static final String TAG = "TAG";
+    EditText mFullName,mEmail,mPassword,mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+//    ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
